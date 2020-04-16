5a519778995c2a3a3c05758cf76bd15a857751e7
Gustavo Rodrigues <kust44@github.com>
2020-04-16 13:13:05 -0300

    Adicionado o card de Professor

--- routes.js
+++ routes.js
@@ -5,6 +5,10 @@ routes.get('/', (req, res) => {
     return res.render("index");
 });
 
+routes.get('/teachers', (req, res) => {
+    return res.render("teachers/index");
+});
+
 routes.use(function(req, res) {
     res.status(404).render("not-found");
 });
